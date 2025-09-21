dcdb319f849563d1025c56e5d6adf1c0ee4b967d
Lucy Mpho Shai <kgorutla9@gmail.com>
2025-09-21 19:51:08 +0200

    Update dbConnection.js

--- Dashboard/projectServer/dbConnection.js
+++ Dashboard/projectServer/dbConnection.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'finances_app',
+  database: process.env.DB_NAME || 'financeDB',
   waitForConnections: true,
   connectionLimit: 1,
   queueLimit: 0
