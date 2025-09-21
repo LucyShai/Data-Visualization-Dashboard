ea71de56baefb7ce144f0ff4551d275f81f547a5
Lucy Mpho Shai <kgorutla9@gmail.com>
2025-09-21 18:46:42 +0200

    Update dbConnection.js

--- Dashboard/projectServer/dbConnection.js
+++ Dashboard/projectServer/dbConnection.js
@@ -1,1 +1,15 @@
-a
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'finances_app',
+  waitForConnections: true,
+  connectionLimit: 1,
+  queueLimit: 0
+});
+
+module.exports = pool;
